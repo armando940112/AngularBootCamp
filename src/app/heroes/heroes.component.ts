479adca47083a970f737df1bb21bec73f7ee596c
ALondono <armando.londono@yuxipacific.com>
2018-02-12 14:37:18 -0500

    Creating hero list, hero details and messages functionalities.

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,29 @@
+import { HeroService } from './../hero.service';
+import { HEROES } from './../mock-heroes';
+import { Hero } from './../hero';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-heroes',
+	templateUrl: './heroes.component.html',
+	styleUrls: [ './heroes.component.css' ]
+})
+export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+  selectedHero: Hero;
+
+	constructor(private heroService: HeroService) {}
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+}
